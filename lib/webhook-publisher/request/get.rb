c955444971d7021dcb4032ae0dfdf0f03332270d
Yuya Kato <yuyakato@gmail.com>
2009-06-23 02:15:43 +0900

    WebHookPublisher::Request::Getクラスを追加

---
+++ lib/webhook-publisher/request/get.rb
@@ -0,0 +1,5 @@
+
+require "webhook-publisher/request/base"
+
+class WebHookPublisher::Request::Get < WebHookPublisher::Request::Base
+end
---
+++ test/request_get.rb
@@ -0,0 +1,6 @@
+
+require File.dirname(__FILE__) + "/test_helper"
+require "webhook-publisher/request/get"
+
+class RequestGetTest < Test::Unit::TestCase
+end
