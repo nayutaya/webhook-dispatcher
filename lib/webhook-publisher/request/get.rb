d8bb42bee62bfb10d9d0dccd822b1bb81001a016
Yuya Kato <yuyakato@gmail.com>
2009-06-23 02:18:35 +0900

    初期化処理を追加

--- lib/webhook-publisher/request/get.rb
+++ lib/webhook-publisher/request/get.rb
@@ -2,4 +2,7 @@
 require "webhook-publisher/request/base"
 
 class WebHookPublisher::Request::Get < WebHookPublisher::Request::Base
+  def initialize(uri)
+    super(uri)
+  end
 end
--- test/request_get.rb
+++ test/request_get.rb
@@ -3,4 +3,12 @@ require File.dirname(__FILE__) + "/test_helper"
 require "webhook-publisher/request/get"
 
 class RequestGetTest < Test::Unit::TestCase
+  def setup
+    @klass = WebHookPublisher::Request::Get
+  end
+
+  def test_initialize
+    req = @klass.new(URI.parse("http://example.jp"))
+    assert_equal(URI.parse("http://example.jp"), req.uri)
+  end
 end
