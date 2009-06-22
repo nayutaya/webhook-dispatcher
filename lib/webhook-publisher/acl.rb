2f514edb3756b3a06666220c88e1aebe7363efc0
Yuya Kato <yuyakato@gmail.com>
2009-06-22 17:20:10 +0900

    IPAddrオブジェクトへの変換処理を追加

--- lib/webhook-publisher/acl.rb
+++ lib/webhook-publisher/acl.rb
@@ -6,17 +6,26 @@ class WebHookPublisher::Acl
     @list = []
   end
 
+  def self.to_ipaddr(ipaddr)
+    case ipaddr
+    when :all   then IPAddr.new("0.0.0.0/0")
+    when String then IPAddr.new(ipaddr)
+    when IPAddr then ipaddr
+    else raise(ArgumentError, "invalid IP address")
+    end
+  end
+
   def size
     return @list.size
   end
 
   def add_allow(ipaddr)
-    @list << [:allow, ipaddr]
+    @list << [:allow, self.class.to_ipaddr(ipaddr)]
     return self
   end
 
   def add_deny(ipaddr)
-    @list << [:deny, ipaddr]
+    @list << [:deny, self.class.to_ipaddr(ipaddr)]
     return self
   end
 
--- test/acl_test.rb
+++ test/acl_test.rb
@@ -14,56 +14,80 @@ class AclTest < Test::Unit::TestCase
     assert_equal(0, acl.size)
   end
 
+  #
+  # クラスメソッド
+  #
+
+  def test_self_to_ipaddr
+    assert_equal(
+      IPAddr.new("127.0.0.0/8"),
+      @klass.to_ipaddr(IPAddr.new("127.0.0.0/8")))
+    assert_equal(
+      IPAddr.new("127.0.0.0/8"),
+      @klass.to_ipaddr("127.0.0.0/8"))
+    assert_equal(
+      IPAddr.new("0.0.0.0/0"),
+      @klass.to_ipaddr(:all))
+
+    assert_raise(ArgumentError) {
+      @klass.to_ipaddr(:invalid)
+    }
+  end
+
+  #
+  # インスタンスメソッド
+  #
+
   def test_add_allow
     assert_equal(0, @acl.size)
-    @acl.add_allow(IPAddr.new("0.0.0.0/0"))
+    @acl.add_allow("0.0.0.0/0")
     assert_equal(1, @acl.size)
-    @acl.add_allow(IPAddr.new("0.0.0.0/0"))
+    @acl.add_allow("0.0.0.0/0")
     assert_equal(2, @acl.size)
   end
 
   def test_add_deny
     assert_equal(0, @acl.size)
-    @acl.add_deny(IPAddr.new("0.0.0.0/0"))
+    @acl.add_deny("0.0.0.0/0")
     assert_equal(1, @acl.size)
-    @acl.add_deny(IPAddr.new("0.0.0.0/0"))
+    @acl.add_deny("0.0.0.0/0")
     assert_equal(2, @acl.size)
   end
 
   def test_allow?
-    assert_equal(true, @acl.allow?(IPAddr.new("127.0.0.1")))
-    @acl.add_deny(IPAddr.new("127.0.0.0/8"))
-    assert_equal(false, @acl.allow?(IPAddr.new("127.0.0.1")))
+    assert_equal(true, @acl.allow?("127.0.0.1"))
+    @acl.add_deny("127.0.0.0/8")
+    assert_equal(false, @acl.allow?("127.0.0.1"))
   end
 
   def test_deny?
-    assert_equal(false, @acl.deny?(IPAddr.new("127.0.0.1")))
-    @acl.add_deny(IPAddr.new("127.0.0.0/8"))
-    assert_equal(true, @acl.deny?(IPAddr.new("127.0.0.1")))
+    assert_equal(false, @acl.deny?("127.0.0.1"))
+    @acl.add_deny("127.0.0.0/8")
+    assert_equal(true, @acl.deny?("127.0.0.1"))
   end
 
   def test_complex1
-    @acl.add_deny(IPAddr.new("0.0.0.0/0"))
-    @acl.add_allow(IPAddr.new("127.0.0.0/8"))
+    @acl.add_deny("0.0.0.0/0")
+    @acl.add_allow("127.0.0.0/8")
 
-    assert_equal(false, @acl.allow?(IPAddr.new("126.255.255.255")))
-    assert_equal(true,  @acl.allow?(IPAddr.new("127.0.0.0")))
-    assert_equal(true,  @acl.allow?(IPAddr.new("127.255.255.255")))
-    assert_equal(false, @acl.allow?(IPAddr.new("128.0.0.0")))
+    assert_equal(false, @acl.allow?("126.255.255.255"))
+    assert_equal(true,  @acl.allow?("127.0.0.0"))
+    assert_equal(true,  @acl.allow?("127.255.255.255"))
+    assert_equal(false, @acl.allow?("128.0.0.0"))
   end
 
   def test_complex2
-    @acl.add_deny(IPAddr.new("0.0.0.0/0"))
-    @acl.add_allow(IPAddr.new("192.168.1.0/24"))
-    @acl.add_allow(IPAddr.new("192.168.3.0/24"))
-    @acl.add_deny(IPAddr.new("192.168.1.127"))
+    @acl.add_deny("0.0.0.0/0")
+    @acl.add_allow("192.168.1.0/24")
+    @acl.add_allow("192.168.3.0/24")
+    @acl.add_deny("192.168.1.127")
 
-    assert_equal(false, @acl.allow?(IPAddr.new("192.168.0.0")))
-    assert_equal(true,  @acl.allow?(IPAddr.new("192.168.1.0")))
-    assert_equal(false, @acl.allow?(IPAddr.new("192.168.1.127")))
-    assert_equal(true,  @acl.allow?(IPAddr.new("192.168.1.255")))
-    assert_equal(false, @acl.allow?(IPAddr.new("192.168.2.0")))
-    assert_equal(true,  @acl.allow?(IPAddr.new("192.168.3.0")))
-    assert_equal(true,  @acl.allow?(IPAddr.new("192.168.3.255")))
+    assert_equal(false, @acl.allow?("192.168.0.0"))
+    assert_equal(true,  @acl.allow?("192.168.1.0"))
+    assert_equal(false, @acl.allow?("192.168.1.127"))
+    assert_equal(true,  @acl.allow?("192.168.1.255"))
+    assert_equal(false, @acl.allow?("192.168.2.0"))
+    assert_equal(true,  @acl.allow?("192.168.3.0"))
+    assert_equal(true,  @acl.allow?("192.168.3.255"))
   end
 end
