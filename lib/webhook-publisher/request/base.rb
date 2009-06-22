e2705f6c7b1541646f4ea0437004117d5c2937f0
Yuya Kato <yuyakato@gmail.com>
2009-06-23 01:17:14 +0900

    初期化処理と属性を追加

--- lib/webhook-publisher/request/base.rb
+++ lib/webhook-publisher/request/base.rb
@@ -1,6 +1,13 @@
 
+require "uri"
+
 class WebHookPublisher::Request
 end
 
 class WebHookPublisher::Request::Base
+  def initialize(uri)
+    @uri = uri
+  end
+
+  attr_accessor :uri
 end
--- test/request_base.rb
+++ test/request_base.rb
@@ -9,6 +9,13 @@ class RequestBaseTest < Test::Unit::TestCase
   end
 
   def test_initialize
-    @klass.new
+    req = @klass.new(URI.parse("http://example.jp"))
+    assert_equal(URI.parse("http://example.jp"), req.uri)
+  end
+
+  def test_uri_attr
+    req = @klass.new(URI.parse("http://example.com"))
+    req.uri = URI.parse("http://example.org")
+    assert_equal(URI.parse("http://example.org"), req.uri)
   end
 end
