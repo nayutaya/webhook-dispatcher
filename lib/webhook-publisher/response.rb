7acd16111f00d3cef7f24bcf1fe8618275fc6754
Yuya Kato <yuyakato@gmail.com>
2009-06-23 00:40:55 +0900

    初期化処理を追加

--- lib/webhook-publisher/response.rb
+++ lib/webhook-publisher/response.rb
@@ -1,15 +1,18 @@
 
 class WebHookPublisher::Response
-  def initialize
-    @status    = :unknown
-    @http_code = nil
-    @message   = nil
-    @exception = nil
+  def initialize(options = {})
+    options = options.dup
+    @success   = (options.delete(:success) == true)
+    @status    = options.delete(:status)    || :unknown
+    @http_code = options.delete(:http_code) || nil
+    @message   = options.delete(:message)   || nil
+    @exception = options.delete(:exception) || nil
+    raise(ArgumentError, "invalid parameter") unless options.empty?
   end
 
-  attr_reader :status, :http_code, :message, :exception
+  attr_reader :success, :status, :http_code, :message, :exception
 
   def success?
-    return false
+    return self.success
   end
 end
--- test/response_test.rb
+++ test/response_test.rb
@@ -9,11 +9,33 @@ class ResponseTest < Test::Unit::TestCase
   end
 
   def test_initialize
+    res = @klass.new(
+      :success   => true,
+      :status    => :success,
+      :http_code => 200,
+      :message   => "OK",
+      :exception => RuntimeError.new)
+    assert_equal(true,     res.success)
+    assert_equal(true,     res.success?)
+    assert_equal(:success, res.status)
+    assert_equal(200,      res.http_code)
+    assert_equal("OK",     res.message)
+    assert_kind_of(RuntimeError, res.exception)
+  end
+
+  def test_initialize__default
     res = @klass.new
+    assert_equal(false,    res.success)
     assert_equal(false,    res.success?)
     assert_equal(:unknown, res.status)
     assert_equal(nil,      res.http_code)
     assert_equal(nil,      res.message)
     assert_equal(nil,      res.exception)
+  end
+
+  def test_initialize__invalid_parameter
+    assert_raise(ArgumentError) {
+      @klass.new(:invalid => true)
+    }
   end
 end
