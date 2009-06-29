a35c479d6190939531f3596bed1af759ca322323
Yuya Kato <yuyakato@gmail.com>
2009-06-30 02:59:11 +0900

    リファクタリング

--- test/request_base_test.rb
+++ test/request_base_test.rb
@@ -7,13 +7,22 @@ class RequestBaseTest < Test::Unit::TestCase
     @klass = WebHookDispatcher::Request::Base
   end
 
+  #
+  # 初期化
+  #
+
   def test_initialize
     req = @klass.new(URI.parse("http://example.jp"))
-    assert_equal(URI.parse("http://example.jp"), req.uri)
+    assert_equal(URI.parse("http://example.jp"), req.instance_eval { @uri })
   end
 
-  def test_uri_attr
+  #
+  # インスタンスメソッド
+  #
+
+  def test_uri
     req = @klass.new(URI.parse("http://example.com"))
+    assert_equal(URI.parse("http://example.com"), req.uri)
     req.uri = URI.parse("http://example.org")
     assert_equal(URI.parse("http://example.org"), req.uri)
   end
