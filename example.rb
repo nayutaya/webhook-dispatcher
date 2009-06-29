fc12b8242d5e4abdb5cb7d6dc8ae686242fc875e
Yuya Kato <yuyakato@gmail.com>
2009-06-30 00:53:41 +0900

    実装済みの機能を削除

--- example.rb
+++ example.rb
@@ -13,23 +13,8 @@ wp.open_timeout = 5 # sec
 wp.read_timeout = 5 # sec
 wp.user_agent = "hoge"
 wp.acl = WebHookPublisher::Acl.with {
-  allow :all
-  allow "*", :all
-  allow "*", 1024..3000
-  allow "*", 0..1024
-  allow "*", [1,2,3,4]
-  deny IPAdd.new("0.0.0.0/0")
-  allow "127.0.0.0/8"
-  allow "localhost"
+  #..
 }
-
-acl.clear
-acl.add_deny(...)
-acl.add_allow(...)
-acl.allow?(ipaddr)
-acl.deny?(ipaddr)
-acl.with { ... }
-
 
 request_obj = WebHookPublisher::Request.new(:get, URI.new(..))
 request_obj.http_method = :get
