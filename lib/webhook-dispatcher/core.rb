b154b7750e34516994e047fec4f7058c3aada199
Yuya Kato <yuyakato@gmail.com>
2009-06-30 01:04:36 +0900

    リファクタリング

--- lib/webhook-dispatcher/core.rb
+++ lib/webhook-dispatcher/core.rb
@@ -4,27 +4,7 @@ class WebHookDispatcher
   @read_timeout = nil
   @user_agent   = nil
 
-  def self.open_timeout
-    return @open_timeout
-  end
-
-  def self.open_timeout=(sec)
-    @open_timeout = sec
-  end
-
-  def self.read_timeout
-    return @read_timeout
-  end
-
-  def self.read_timeout=(sec)
-    @read_timeout = sec
-  end
-
-  def self.user_agent
-    return nil
-  end
-
-  def self.user_agent=(value)
-    @user_agent = value
+  class << self
+    attr_accessor :open_timeout, :read_timeout, :user_agent
   end
 end
