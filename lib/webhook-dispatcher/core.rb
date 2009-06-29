9bf828d6ec1f08dab605bb23afc519ea1a9469e9
Yuya Kato <yuyakato@gmail.com>
2009-06-30 01:10:34 +0900

    プロパティを追加

--- lib/webhook-dispatcher/core.rb
+++ lib/webhook-dispatcher/core.rb
@@ -4,7 +4,15 @@ class WebHookDispatcher
   @read_timeout = nil
   @user_agent   = nil
 
+  def initialize
+    @open_timeout = nil
+    @read_timeout = nil
+    @user_agent   = nil
+  end
+
   class << self
     attr_accessor :open_timeout, :read_timeout, :user_agent
   end
+
+  attr_accessor :open_timeout, :read_timeout, :user_agent
 end
--- test/core_test.rb
+++ test/core_test.rb
@@ -5,6 +5,7 @@ require "webhook-dispatcher/core"
 class CoreTest < Test::Unit::TestCase
   def setup
     @klass = WebHookDispatcher
+    @dispatcher = @klass.new
   end
 
   #
@@ -26,5 +27,24 @@ class CoreTest < Test::Unit::TestCase
   def test_self_user_agent
     assert_equal(nil, @klass.user_agent)
     @klass.user_agent = "ie"
+  end
+
+  #
+  # インスタンスメソッド
+  #
+
+  def test_open_timeout
+    @dispatcher.open_timeout = 10
+    assert_equal(10, @dispatcher.open_timeout)
+  end
+
+  def test_read_timeout
+    @dispatcher.read_timeout = 10
+    assert_equal(10, @dispatcher.read_timeout)
+  end
+
+  def test_user_agent
+    @dispatcher.user_agent = "firefox"
+    assert_equal("firefox", @dispatcher.user_agent)
   end
 end
