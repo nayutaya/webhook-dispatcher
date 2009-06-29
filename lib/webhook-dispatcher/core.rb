02483a48db0e142edf9271424c87d1c4efdf0ecd
Yuya Kato <yuyakato@gmail.com>
2009-06-30 01:33:29 +0900

    リファクタリング

--- lib/webhook-dispatcher/core.rb
+++ lib/webhook-dispatcher/core.rb
@@ -2,10 +2,6 @@
 require "webhook-dispatcher/version"
 
 class WebHookDispatcher
-  @open_timeout = nil
-  @read_timeout = nil
-  @user_agent   = nil
-
   def initialize(options = {})
     options = options.dup
     @open_timeout = options.delete(:open_timeout) || self.class.open_timeout || self.class.default_open_timeout
@@ -14,19 +10,13 @@ class WebHookDispatcher
   end
 
   class << self
+    @open_timeout = nil
+    @read_timeout = nil
+    @user_agent   = nil
     attr_accessor :open_timeout, :read_timeout, :user_agent
-  end
-
-  def self.default_open_timeout
-    return 10
-  end
-
-  def self.default_read_timeout
-    return 10
-  end
-
-  def self.default_user_agent
-    return "webhook-dispatcher #{self::VERSION}"
+    define_method(:default_open_timeout) { 10 }
+    define_method(:default_read_timeout) { 10 }
+    define_method(:default_user_agent)   { "webhook-dispatcher #{self::VERSION}" }
   end
 
   attr_accessor :open_timeout, :read_timeout, :user_agent
